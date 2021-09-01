320120a11e87f1bb2a1e5eae40b491715d527f8a
zod <zod@zods-MacBook-Air.local>
2021-09-01 16:28:53 -0400

    remove handler

--- pages/api/bags/index.ts
+++ pages/api/bags/index.ts
@@ -71,13 +71,3 @@ export const fetchBags = async (lootItem) => {
   }
 }
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const data = await fetchBags()
-    res.status(200).json(data)
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
-  }
-}
-
-export default handler
